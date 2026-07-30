b0131b1d3ee352f460facaab9ece631a0b729ce5
Uolrier <2940393207@qq.com>
2026-07-30 13:19:09 +0800

    add local runtime abstract interface

---
+++ backend/app/runtime/__init__.py
@@ -0,0 +1,5 @@
+from .base import BaseRuntime
+
+__all__ = [
+    "BaseRuntime",
+]
---
+++ backend/app/runtime/base.py
@@ -0,0 +1,30 @@
+from abc import ABC, abstractmethod
+
+
+class BaseRuntime(ABC):
+    """
+    Local inference runtime abstract interface.
+
+    All local model runtimes should inherit from this class.
+    """
+
+    @abstractmethod
+    def load(self):
+        """
+        Load model resources.
+        """
+        pass
+
+    @abstractmethod
+    def generate(self, prompt: str, **kwargs):
+        """
+        Generate response from model.
+        """
+        pass
+
+    @abstractmethod
+    def unload(self):
+        """
+        Release model resources.
+        """
+        pass
